effd97aee93cfbdc9fe670724c7766e17f8632b3
Warkentin Erwin <warkentinerwin@icloud.com>
2023-05-24 19:35:49 +0200

    changed project to SwiftUI

---
+++ UnitConversion/UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Erwin Warkentin on 24.05.23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
